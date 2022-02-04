3ab3adffb15a12cd12715a795d2bc28c2f45b972
eunyoung <72347107+eunyoung56@users.noreply.github.com>
2022-02-04 09:31:24 +0900

    루시와 엘라 찾기 ( String, Date ) 수정

--- Algorithm/SQL/StringDate.sql
+++ Algorithm/SQL/StringDate.sql
@@ -1,6 +1,18 @@
 Q. 동물 보호소에 들어온 동물 중 이름이 Lucy, Ella, Pickle, Rogan, Sabrina, Mitty인   
 동물의 아이디와 이름, 성별 및 중성화 여부를 조회하는 SQL 문을 작성해주세요.
-A. -- 코드를 입력하세요
+A. -- 코드를 입력하세요.  
+Answer
+SELECT ANIMAL_ID,NAME,SEX_UPON_INTAKE
+FROM ANIMAL_INS
+WHERE NAME LIKE 'Lucy'
+OR NAME LIKE 'Ella'
+OR NAME LIKE 'Pickle'
+OR NAME LIKE 'Rogan'
+OR NAME LIKE 'Sabrina'
+OR NAME LIKE 'Mitty'
+ORDER BY ANIMAL_ID
+
+Wrong Answer
 SELECT ANIMAL_ID,NAME,SEX_UPON_INTAKE
 FROM ANIMAL_INS
 WHERE NAME LIKE '%Lucy%'
@@ -10,4 +22,3 @@ OR NAME LIKE '%Rogan%'
 OR NAME LIKE '%Sabrina%'
 OR NAME LIKE '%Mitty%'
 
-WHERE NAME LIKE IN ( Lucy , Ella , Pickle, Rogan, Sabrina )
