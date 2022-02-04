9c73a5e7f9b960d4e0536e4dbb04af1482fbb40c
eunyoung <72347107+eunyoung56@users.noreply.github.com>
2022-02-04 09:34:49 +0900

    이름에 el이 들어가는 동물 찾기 ( like, = ) 수정

--- Algorithm/SQL/StringDate1.sql
+++ Algorithm/SQL/StringDate1.sql
@@ -1,9 +1,16 @@
-Q. 이름에 el이 들어가는 동물 찾기
+Q. 보호소에 돌아가신 할머니가 기르던 개를 찾는 사람이 찾아왔습니다. 이 사람이 말하길 할머니가 기르던 개는 이름에 'el'이 들어간다고 합니다.  
+동물 보호소에 들어온 동물 이름 중, 이름에 "EL"이 들어가는 개의 아이디와 이름을 조회하는 SQL문을 작성해주세요.  
+이때 결과는 이름 순으로 조회해주세요. 단, 이름의 대소문자는 구분하지 않습니다.
 A. -- 코드를 입력하세요
+Answer
 SELECT ANIMAL_ID,NAME
 FROM ANIMAL_INS
-WHERE upper(NAME) like upper('%EL%')
+WHERE NAME like ('%EL%')
+AND ANIMAL_TYPE = "Dog"
 ORDER BY NAME
 
-WHERE NAME LIKE "%EL%"
-수정중..
+Wrong Answer
+SELECT ANIMAL_ID,NAME
+FROM ANIMAL_INS
+WHERE upper(NAME) like upper('%EL%') <-대소문자 구분
+ORDER BY NAME
