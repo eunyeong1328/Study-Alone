a8ee43cdf66733d2d8a4da2d1424db54bf284032
eunyoung <72347107+eunyoung56@users.noreply.github.com>
2022-02-08 21:45:39 +0900

    오랜 기간 보호한 동물(2) ( LIMIT )

--- Algorithm/SQL/StringDate3.sql
+++ Algorithm/SQL/StringDate3.sql
@@ -2,7 +2,11 @@ Q. 입양을 간 동물 중, 보호 기간이 가장 길었던 동물 두 마리
 이때 결과는 보호 기간이 긴 순으로 조회해야 합니다. 
 
 A. -- 코드를 입력하세요
-SELECT ANIMAL_ID,NAME, MAX(DATETIME)
-FROM ANIMAL_INS I
-NATURAL JOIN ANIMAL_OUTS O
-WHERE I.ANIMAL_ID = O.ANIMAL_ID
+-- 코드를 입력하세요
+SELECT ANIMAL_ID,NAME
+FROM ANIMAL_INS
+ORDER BY DATETIME 
+LIMIT 0,2
+
+LIMIT : 데이터 조회 시 한계를 지정할 수 있으며 위의 쿼리 LIMIT 은 0부터 2까지의 행의 데이터를 가져온다.
+ROWNUM : 조회된 결과의 순서대로 순서를 붙여준다.
